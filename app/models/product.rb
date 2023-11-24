d5274cf8e80aee6f2d8fa68904bb114c080d5346
Eddie Kao <eddie@5xcampus.com>
2023-11-24 16:50:22 +0800

    add soft deleted

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, numericality: { greater_than: 0 }
+
+  # scope
+  default_scope { where(deleted_at: nil) }
+
+  def destroy
+    update(deleted_at: Time.current)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_070046) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_24_074325) do
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_070046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "onsale", default: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_products_on_deleted_at"
   end
 
 end
