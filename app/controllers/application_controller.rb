c5f929f55defa82cefde2f285a41c37e31eedb1f
Eddie Kao <eddie@5xcampus.com>
2023-11-24 14:55:34 +0800

    add rescue_from for record not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found
+    render file: Rails.public_path.join('404.html'),
+           status: 404,
+           layout: false
+  end
 end
