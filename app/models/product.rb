9ce81b4225aed6cd4353321f84c9eb9e84ad510b
Eddie Kao <eddie@5xcampus.com>
2023-11-24 16:56:09 +0800

    use paranoia gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,6 @@
 class Product < ApplicationRecord
+  acts_as_paranoid
+
   validates :title, presence: true
   validates :price, numericality: { greater_than: 0 }
-
-  # scope
-  default_scope { where(deleted_at: nil) }
-
-  def destroy
-    update(deleted_at: Time.current)
-  end
 end
