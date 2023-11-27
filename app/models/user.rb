de171fa4e30f8a06c5a5e01647c0fa7aa743a28f
Eddie Kao <eddie@5xcampus.com>
2023-11-27 12:43:23 +0800

    encrypt password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
+
+  before_create :encrypt_password
+  # 2FA 二階段驗證
+
+  private
+
+  def encrypt_password
+    salted_password = "*xx#{self.password}yy-"
+    self.password = Digest::SHA256.hexdigest(salted_password)
+  end
 end
