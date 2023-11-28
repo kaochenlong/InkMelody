9d024ad44792db1805d74a5b314239b4eeb7baa7
Eddie Kao <eddie@5xcampus.com>
2023-11-28 14:47:02 +0800

    add find_owned_product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :find_product, only: [:show, :edit, :update, :destroy]
+  before_action :find_product, only: [:show]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_owned_product, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.order(id: :desc)
@@ -49,5 +50,9 @@ class ProductsController < ApplicationController
 
   def find_product
     @product = Product.find(params[:id])
+  end
+
+  def find_owned_product
+    @product = current_user.products.find(params[:id])
   end
 end
