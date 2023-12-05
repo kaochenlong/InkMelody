b8162f2e601a990eb1d2d608311442755fd3ad4c
Eddie Kao <eddie@5xcampus.com>
2023-12-05 17:09:54 +0800

    add product list

---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,9 @@
+module ProductsHelper
+  def product_cover(product, variant = :thumb)
+    if product.cover.attached?
+      image_tag product.cover.variant(variant)
+    else
+      image_tag "/images/products/default.jpg"
+    end
+  end
+end
