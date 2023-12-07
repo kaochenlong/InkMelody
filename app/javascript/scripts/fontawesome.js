c486391071b69268aa18d1c07129383165536097
Eddie Kao <eddie@5xcampus.com>
2023-12-07 15:14:10 +0800

    add font awesome

---
+++ app/javascript/scripts/fontawesome.js
@@ -0,0 +1,5 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faHeart, faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHeart, faCartShopping);
+dom.watch();
