852d6166cad1fa93232d98db47ee50a964f2083e
Eddie Kao <eddie@5xcampus.com>
2023-12-08 14:33:48 +0800

    add stimulus controller for braintree dropin

--- app/javascript/controllers/braintree_controller.js
+++ app/javascript/controllers/braintree_controller.js
@@ -3,19 +3,41 @@ import dropin from "braintree-web-drop-in";
 
 // Connects to data-controller="braintree"
 export default class extends Controller {
-  connect() {
+  async connect() {
+    const form = this.element.closest("form");
+
+    // disable submit button
+    const submitButton = form.querySelector("input[type=submit]");
+    if (submitButton) {
+      submitButton.disabled = true;
+    }
+
     const { token } = this.element.dataset;
 
-    dropin
-      .create({
-        container: this.element,
-        authorization: token,
-      })
-      .then((instance) => {
-        console.log(instance);
-      })
-      .catch((err) => {
-        console.log(err);
+    const instance = await dropin.create({
+      container: this.element,
+      authorization: token,
+    });
+
+    if (instance && submitButton) {
+      submitButton.disabled = false;
+    }
+
+    if (form) {
+      form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+
+        const { nonce } = await instance.requestPaymentMethod();
+        if (nonce) {
+          const nonce_field = document.createElement("input");
+          nonce_field.setAttribute("type", "hidden");
+          nonce_field.setAttribute("name", "nonce");
+          nonce_field.setAttribute("value", nonce);
+          form.appendChild(nonce_field);
+
+          form.submit();
+        }
       });
+    }
   }
 }
