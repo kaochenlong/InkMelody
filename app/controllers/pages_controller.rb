92ed3de7efc08efec422690d38cef8dd0a0f6e26
Eddie Kao <eddie@5xcampus.com>
2023-12-11 10:15:01 +0800

    update styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
 
   def about
-    # 實體變數
-    @numbers = (1..42).to_a.sample(5)
   end
 
   def privacy
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,6 @@ module.exports = {
   ],
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "pastel"],
+    themes: ["light", "dark", "pastel", "lofi"],
   },
 };
