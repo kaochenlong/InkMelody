fb536718d7c2e35541e23fc04982ba436834b822
Eddie Kao <eddie@5xcampus.com>
2023-12-11 16:06:19 +0800

    replace with tag helper

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      redirect_to product_path(@product), notice: '更新成功'
+      redirect_to my_products_path, notice: '更新成功'
     else
       render :edit
     end
@@ -70,6 +70,6 @@ class ProductsController < ApplicationController
   end
 
   def find_owned_product
-    @product = current_user.products.find(params[:id])
+    @product = current_user.products.unscope(:where).find(params[:id])
   end
 end
