8228f14f05e429d81b7f04e276a8afd66702a9d5
Eddie Kao <eddie@5xcampus.com>
2023-12-12 12:52:29 +0800

    add i18n files

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module InkMelody
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.i18n.default_locale = :tw
 
     # Configuration for the application, engines, and railties goes here.
     #
